e8bbab1025e203393b876ab8885e90b3e7119e3a
tim@covertnine.com <tim@covertnine.com>
2016-03-17 15:37:09 -0400

    initial commit;

---
+++ functions.php
@@ -0,0 +1,22 @@
+<?php
+
+/* load up stylesheets in proper order*/
+function cortex_enqueue_child_styles() {
+
+    //grab theme options
+    $cortex_theme_options = thsp_cbp_get_options_values();
+
+    //queue up the child stylesheet after the dark/light theme style
+    if ( $cortex_theme_options['cortex_theme_style'] == 'light' ) {
+
+        wp_enqueue_style( 'cortex-child-style', get_stylesheet_directory_uri() . '/style.css', array( 'cortex-style-light' ) );
+
+    } else {
+
+        wp_enqueue_style( 'cortex-child-style', get_stylesheet_directory_uri() . '/style.css', array( 'cortex-style-dark' ) );
+
+    }
+
+    add_editor_style( get_stylesheet_directory_uri() . '/style.css');
+}
+add_action( 'wp_enqueue_scripts', 'cortex_enqueue_child_styles', 25 );
