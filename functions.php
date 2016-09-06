913b32f1542c1230b259c8abbd9b85956c983ff6
tim@covertnine.com <tim@covertnine.com>
2016-09-06 18:19:24 -0400

    updated for redux-options framework

--- functions.php
+++ functions.php
@@ -3,17 +3,17 @@
 /* load up stylesheets in proper order*/
 function cortex_enqueue_child_styles() {
 
-    //grab theme options
-    $cortex_theme_options = thsp_cbp_get_options_values();
+	//grab theme options
+	$cortex_theme_options = global $cortex_options;
 
     //queue up the child stylesheet after the dark/light theme style
-    if ( $cortex_theme_options['cortex_theme_style'] == 'light' ) {
+	if ( $cortex_theme_options['c9-theme-style'] == 'light' ) {
 
-        wp_enqueue_style( 'cortex-child-style', get_stylesheet_directory_uri() . '/style.css', array( 'cortex-style-light' ) );
+    	wp_enqueue_style( 'cortex-child-style', get_stylesheet_directory_uri() . '/style.css', array( 'cortex-style-light' ) );
 
     } else {
 
-        wp_enqueue_style( 'cortex-child-style', get_stylesheet_directory_uri() . '/style.css', array( 'cortex-style-dark' ) );
+    	wp_enqueue_style( 'cortex-child-style', get_stylesheet_directory_uri() . '/style.css', array( 'cortex-style-dark' ) );
 
     }
 
