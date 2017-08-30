48b384b3badc5203205af4059544f7d1c6cfd4a7
Tim <tim@covertnine.com>
2017-08-30 16:16:20 -0400

    field updates to match parent, adjustment to stylesheet queuing

--- functions.php
+++ functions.php
@@ -9,20 +9,22 @@ function cortex_enqueue_child_styles() {
     //queue up the child stylesheet after the dark/light theme style
 	if ( $cortex_options['c9-theme-style'] == 'light' ) {
 
-    	wp_enqueue_style( 'cortex-child-style', get_stylesheet_directory_uri() . '/style.css', array( 'cortex-style-light' ) );
+    	wp_enqueue_style( 'cortex-child-style', get_stylesheet_directory_uri() . '/style.css', array( 'cortex-style-colors' ) );
 
     } else {
 
-    	wp_enqueue_style( 'cortex-child-style', get_stylesheet_directory_uri() . '/style.css', array( 'cortex-style-dark' ) );
+    	wp_enqueue_style( 'cortex-child-style', get_stylesheet_directory_uri() . '/style.css', array( 'cortex-style-colors' ) );
 
     }
 
-    add_editor_style( get_stylesheet_directory_uri() . '/style.css');
 }
-add_action( 'wp_enqueue_scripts', 'cortex_enqueue_child_styles', 35 );
+add_action( 'wp_enqueue_scripts', 'cortex_enqueue_child_styles', 100 );
 
 /* add child style to editor */
 function cortex_child_editor_style() {
-   add_editor_style( get_stylesheet_directory_uri() . '/style.css' );
+	add_editor_style( get_template_directory_uri() . '/style.css' );
+	add_editor_style( get_stylesheet_directory_uri() . '/style.css' );
 }
-add_action( 'init', 'cortex_child_editor_style' );
+add_action( 'admin_init', 'cortex_child_editor_style', 45 );
+
+//define( 'ACF_LITE' , true );
